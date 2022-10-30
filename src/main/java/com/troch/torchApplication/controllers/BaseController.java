bc6ce9911842f93b39ef13631430015d4126b9a4
taiwo <X00167646@myTUDublin.ie>
2022-10-30 15:46:51 +0000

    Directory Change

---
+++ src/main/java/com/troch/torchApplication/controllers/BaseController.java
@@ -0,0 +1,17 @@
+package com.troch.torchApplication.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller
+public class BaseController {
+
+    @GetMapping(value = "/error")
+    public String errorPage(){
+
+        return "error";
+    }
+}
