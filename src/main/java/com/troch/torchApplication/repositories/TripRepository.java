cac3d2e5dc491f19f90a23d7b3e2951d2f8a9fec
taiwo <X00167646@myTUDublin.ie>
2023-01-18 15:56:26 +0000

    CodeBase Commit

--- src/main/java/com/troch/torchApplication/repositories/TripRepository.java
+++
@@ -1,9 +0,0 @@
-package com.troch.torchApplication.repositories;
-
-import com.troch.torchApplication.models.Trip;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TripRepository extends JpaRepository<Trip, Integer> {
-}
