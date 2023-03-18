f11a1d21b04f9d90787ed286ee600b9297bb52e6
taiwo <X00167646@myTUDublin.ie>
2023-03-18 14:03:44 +0000

    updates

---
+++ FrontEnd/src/app/user/components/subscription-modal/subscription-modal.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input, OnInit} from '@angular/core';import {HttpClient} from "@angular/common/http";
+import {StripeService} from "ngx-stripe";
+import {LoadingService} from "../../../shared/services/loading.service";
+import {environment} from "../../../../environments/environment";
+import {switchMap} from "rxjs";
+import {UserService} from "../../services/user.service";
+
+@Component({
+  selector: 'app-subscription-modal',
+  templateUrl: './subscription-modal.component.html',
+  styleUrls: ['./subscription-modal.component.css']
+})
+export class SubscriptionModalComponent implements OnInit{
+  priceId: any;
+  session:any;
+  constructor(
+      private stripeService: StripeService,
+      private loadingService: LoadingService,
+      private userService:UserService
+  ) {
+  }
+
+  ngOnInit() {
+
+  }
+
+    checkout(price_id:any) {
+    this.loadingService.isLoading.next(true);
+    // Check the server.js tab to see an example implementation
+    this.userService.createCheckoutSessionFromAPI(price_id)
+        .subscribe(result => {
+          this.loadingService.isLoading.next(false);
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, you should display the localized error message to your
+          // customer using `error.message`.
+          if (result.error) {
+            this.loadingService.isLoading.next(false);
+            alert(result.error.message);
+          }
+        });
+  }
+}
