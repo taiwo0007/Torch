0088082b17f7b131ffdce045b15cddccdfed8885
taiwo <X00167646@myTUDublin.ie>
2023-04-12 11:25:13 +0100

    trip page loading fix

--- FrontEnd/src/app/escooter/components/escooter-detail/escooter-detail.component.ts
+++ FrontEnd/src/app/escooter/components/escooter-detail/escooter-detail.component.ts
@@ -8,119 +8,123 @@ import {ImageModule} from 'primeng/image';
 import {LoadingService} from "../../../shared/services/loading.service";
 
 @Component({
-  selector: 'app-escooter-detail',
-  templateUrl: './escooter-detail.component.html',
-  styleUrls: ['./escooter-detail.component.css']
+    selector: 'app-escooter-detail',
+    templateUrl: './escooter-detail.component.html',
+    styleUrls: ['./escooter-detail.component.css']
 })
 export class EscooterDetailComponent implements OnInit, AfterViewInit, AfterContentInit {
 
-  paramId: number;
-  isLoading = false;
-  escooter: Escooter;
-  ratingArray:number[];
-  markerPosition:google.maps.LatLngLiteral;
-  isAuthenticated = false;
-  options:google.maps.MapOptions;
-  markerOptions: google.maps.MarkerOptions = {draggable: false, icon: 'assets/images/website/icons/markerChanged.png'};
+    paramId: number;
+    isLoading = false;
+    escooter: Escooter;
+    ratingArray: number[];
+    markerPosition: google.maps.LatLngLiteral;
+    isAuthenticated = false;
+    options: google.maps.MapOptions;
+    markerOptions: google.maps.MarkerOptions = {
+        draggable: false,
+        icon: 'assets/images/website/icons/markerChanged.png'
+    };
 
 
-  constructor(private escooterService: EscooterService,
-              private route:ActivatedRoute,
-              private authService: AuthService,
-              private router:Router, private loadService:LoadingService) { }
-
-  ngOnInit(): void {
-    this.loadService.isLoading.next(true);
-    this.isLoading = true;
-    this.route.params.subscribe( params => {
-      this.paramId = params['id'];
-
-    })
-
-
-    // this.isLoading = true;
-      this.escooterService.getEscooterById(this.paramId).subscribe( escooterData => {
-        // this.isLoading = false;
-      this.escooter = escooterData;
-        this.configureMapOptions();
-        this.configureMarker();
-      console.log(escooterData)
-      this.ratingArray = Array(Math.trunc(escooterData.rating)).fill(0).map((x,i)=>i)
-
-      console.log("Escooter Data"+this.escooter)
-            this.isLoading = false;
-
-
-
-    }, error => {
-        // this.isLoading = false;
-            this.isLoading = false;
-
-        this.router.navigate(['../error'])
-      },
-          ()=> {
-            this.loadService.isLoading.next(false);
-            this.isLoading = false;
-
-          })
-    console.log(this.ratingArray)
-    this.authService.user.subscribe((data:boolean) => this.isAuthenticated = data )
-
-
-    this.escooterService.EscooterChangeEmitter.subscribe(() => {
-
-      this.escooterService.getEscooterById(this.paramId)
-          .subscribe( escooterData => {
-        this.escooter = escooterData;
-            this.isLoading = false;
-            this.loadService.isLoading.next(false);
-
-        this.ratingArray = Array(escooterData.rating).fill(0).map((x,i)=>i)
-      })
-
-    })
-  }
-
-  ngAfterViewInit() {
-
-  }
-
-  ngAfterContentInit() {
-    this.initMap()
-
-  }
-
-  initMap() {
-    var location = { lat: 53.410980, lng: -6.400090 }
-    var options = {
-
-      center: location,
-      zoom: 15
+    constructor(private escooterService: EscooterService,
+                private route: ActivatedRoute,
+                private authService: AuthService,
+                private router: Router, private loadService: LoadingService) {
     }
 
-    const map = new google.maps.Map(document.getElementById("map"), options)
+    ngOnInit(): void {
+        this.loadService.isLoading.next(true);
+        this.isLoading = true;
+        this.route.params.subscribe(params => {
+            this.paramId = params['id'];
 
-    new google.maps.Marker({
-      position: location,
-      map,
-      title: "Hello World!",
-      icon: "assets/images/website/icons/markerChanged.png"
-    });
-  }
-
-  configureMapOptions(){
-    console.log(+this.escooter?.longitude)
-  this.options = {
-      center: {lat: +this.escooter?.latitude, lng: +this.escooter?.longitude},
-      zoom: 15
-    };
-  }
-
-  configureMarker(){
-    console.log(+this.escooter?.longitude)
+        })
 
 
-    this.markerPosition = {lat: +this.escooter?.latitude, lng: +this.escooter?.longitude}
-  }
+        // this.isLoading = true;
+        this.escooterService.getEscooterById(this.paramId).pipe(delay(9000)).subscribe(escooterData => {
+                // this.isLoading = false;
+                this.escooter = escooterData;
+                this.configureMapOptions();
+                this.configureMarker();
+                console.log(escooterData)
+                this.ratingArray = Array(Math.trunc(escooterData.rating)).fill(0).map((x, i) => i)
+
+                console.log("Escooter Data" + this.escooter)
+                this.isLoading = false;
+
+
+            }, error => {
+                // this.isLoading = false;
+                this.isLoading = false;
+                this.loadService.isLoading.next(false);
+
+                this.router.navigate(['../error'])
+            },
+            () => {
+                this.loadService.isLoading.next(false);
+                this.isLoading = false;
+
+            })
+        console.log(this.ratingArray)
+        this.authService.user.subscribe((data: boolean) => this.isAuthenticated = data)
+
+
+        this.escooterService.EscooterChangeEmitter.subscribe(() => {
+
+            this.escooterService.getEscooterById(this.paramId)
+                .subscribe(escooterData => {
+                    this.escooter = escooterData;
+                    this.isLoading = false;
+                    this.loadService.isLoading.next(false);
+
+                    this.ratingArray = Array(escooterData.rating).fill(0).map((x, i) => i)
+                })
+
+        })
+    }
+
+    ngAfterViewInit() {
+
+    }
+
+    ngAfterContentInit() {
+        this.initMap()
+
+    }
+
+    initMap() {
+        var location = {lat: 53.410980, lng: -6.400090}
+        var options = {
+
+            center: location,
+            zoom: 15
+        }
+
+        const map = new google.maps.Map(document.getElementById("map"), options)
+
+        new google.maps.Marker({
+            position: location,
+            map,
+            title: "Hello World!",
+            icon: "assets/images/website/icons/markerChanged.png"
+        });
+    }
+
+    configureMapOptions() {
+        console.log(+this.escooter?.longitude)
+        this.options = {
+            center: {lat: +this.escooter?.latitude, lng: +this.escooter?.longitude},
+            zoom: 15
+        };
+    }
+
+    configureMarker() {
+        console.log(+this.escooter?.longitude)
+
+
+        this.markerPosition = {lat: +this.escooter?.latitude, lng: +this.escooter?.longitude}
+    }
 
 }
--- FrontEnd/src/app/trip/components/trip/trip.component.ts
+++ FrontEnd/src/app/trip/components/trip/trip.component.ts
@@ -5,203 +5,221 @@ import {Trip} from "../../models/trip";
 import {AreYouSureDialogComponent} from "../../../shared/components/are-you-sure-dialog/are-you-sure-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
 import {DialogService} from "../../../shared/services/dialog.service";
-import {filter, of, Subscription, switchMap} from "rxjs";
+import {delay, filter, of, Subscription, switchMap} from "rxjs";
 import {
-  AreYouSureDialogCancelComponent
+    AreYouSureDialogCancelComponent
 } from "../../../shared/components/are-you-sure-dialog-cancel/are-you-sure-dialog-cancel.component";
 import {LoadingService} from "../../../shared/services/loading.service";
 import {ToastrService} from "ngx-toastr";
 
 @Component({
-  selector: 'app-trip',
-  templateUrl: './trip.component.html',
-  styleUrls: ['./trip.component.css']
+    selector: 'app-trip',
+    templateUrl: './trip.component.html',
+    styleUrls: ['./trip.component.css']
 })
 export class TripComponent implements OnInit, AfterViewInit {
-  isNewlyBooked = false;
-  tripId:number;
-  trip: Trip;
-  initialCost: number;
-  vatCost: number;
-  totalCost: number;
-  todaysDate: Date = new Date()
-  tripEndFormattedDate:Date;
-  confirmSubscription:Subscription;
-  cancelSubscription:Subscription;
-  isError;
-  isLoading = false;
+    isNewlyBooked = false;
+    tripId: number;
+    trip: Trip;
+    initialCost: number;
+    vatCost: number;
+    totalCost: number;
+    todaysDate: Date = new Date()
+    tripEndFormattedDate: Date;
+    confirmSubscription: Subscription;
+    cancelSubscription: Subscription;
+    isError;
+    isLoading = false;
 
-  constructor(private route: ActivatedRoute,
-              private tripService: TripService,
-              private dialog: MatDialog,
-              private dialogService:DialogService,
-              private router:Router,
-              private loadingService:LoadingService,
-              private toastr:ToastrService) { }
-
-  ngOnInit(): void {
-    this.intialiseSuccessStatus();
-    this.initTripId();
-    console.log(this.todaysDate)
-    this.isLoading = true;
-    this.confirmSubscription = this.dialogService.confirmTripComplete
-        .pipe(
-            switchMap((val:any) => {
-              this.loadingService.isLoadingLine.next(true);
-this.isLoading = false
-
-              return this.tripService.completeTripFromApi(this.tripId)
-            })
-
-        ).subscribe(
-         (data:Trip) => {
-          console.log(data)
-           this.loadingService.isLoadingLine.next(false);
-           this.isLoading = false
-           this.router.navigate(['/trip-complete'], {queryParams: {tripId: data.tripId}})
-        },()=>{
-          this.isError = "An Error has occurred"
-        },()=>{
-              this.loadingService.isLoadingLine.next(false);
-
-              this.isLoading = false;
-        })
-
-    this.cancelSubscription = this.dialogService.confirmTripCancel
-        .pipe(
-            switchMap((val:any) => {
-              this.loadingService.isLoadingLine.next(true);
-
-              this.isLoading = true;
-              return this.tripService.cancelTripFromApi(this.tripId)
-            })
-
-        ).subscribe(
-            (data:Trip) => {
-              console.log(data)
-              this.loadingService.isLoadingLine.next(false);
-              this.isLoading = false
-
-              this.router.navigate(['/trip-cancel'], {queryParams: {tripId: data.tripId}})
-            },()=>{
-              this.loadingService.isLoadingLine.next(false);
-              this.isLoading = false;
-              this.isError = "An Error has occurred"
-            },()=>{
-              this.loadingService.isLoadingLine.next(false);
-
-              this.isLoading = false;
-            })
-
-       this.isLoading = false
-
-  }
-
-  ngAfterViewInit() {
-    this.calculateTripCost()
-    this.initMap();
-    console.log(this.trip)
-
-  }
-
-  intialiseSuccessStatus(): void{
-    this.route.queryParams.subscribe((queryParams) => {
-      this.isNewlyBooked = queryParams['success'];
-      if(this.isNewlyBooked){
-        this.toastr.success(  'Trip Booked Successfully', '', {
-          positionClass: 'toast-top-center'
-        });
-      }
-
-    }, error1 => {
-      console.log("error", error1)
-    })
-  }
-
-  initTripId(){
-    this.route.params.subscribe(params => {
-      this.tripId = params['id'];
-      console.log(this.tripId)
-      this.fetchTripDetails(this.tripId);
-    })
-  }
-
-  fetchTripDetails(id:number){
-    console.log(id)
-      this.tripService.fetchTripDetailsFromApi(id).subscribe((tripData:any) => {
-        this.tripEndFormattedDate = new Date(tripData.tripEnd);
-      this.trip = tripData;
-      })
-
-  }
-
-  calculateTripCost(){
-    console.log(this.trip.days)
-    this.initialCost = this.trip.days * this.trip.eScooterOnTrip.cost;
-    this.vatCost = this.initialCost * 0.20;
-    this.totalCost = this.initialCost + 20 + this.vatCost;
-  }
-
-  initMap() {
-    var location = { lat: 53.410980, lng: -6.400090 }
-    var options = {
-
-      center: location,
-      zoom: 15
+    constructor(private route: ActivatedRoute,
+                private tripService: TripService,
+                private dialog: MatDialog,
+                private dialogService: DialogService,
+                private router: Router,
+                private loadingService: LoadingService,
+                private toastr: ToastrService) {
     }
 
-    const map = new google.maps.Map(document.getElementById("map"), options)
+    ngOnInit(): void {
 
-    new google.maps.Marker({
-      position: location,
-      map,
-      title: "Hello World!",
-      icon: "assets/images/website/icons/markerChanged.png"
-    });
-  }
+        this.isLoading = true;
+        console.log("loading service placeholder")
+        this.loadingService.isLoading.next(true)
+        this.intialiseSuccessStatus();
+        this.initTripId();
+        console.log(this.todaysDate)
+        this.isLoading = true;
+        this.confirmSubscription = this.dialogService.confirmTripComplete
+            .pipe(
+                switchMap((val: any) => {
+                    this.loadingService.isLoadingLine.next(true);
+                    this.isLoading = false
+
+                    return this.tripService.completeTripFromApi(this.tripId)
+                })
+            ).subscribe(
+                (data: Trip) => {
+                    console.log(data)
+                    this.loadingService.isLoadingLine.next(false);
+                    this.isLoading = false
+                    this.router.navigate(['/trip-complete'], {queryParams: {tripId: data.tripId}})
+                }, () => {
+                    this.isError = "An Error has occurred"
+                }, () => {
+                    this.loadingService.isLoadingLine.next(false);
+
+                    this.isLoading = false;
+                })
+
+        this.cancelSubscription = this.dialogService.confirmTripCancel
+            .pipe(
+                switchMap((val: any) => {
+                    this.loadingService.isLoadingLine.next(true);
+
+                    this.isLoading = true;
+                    return this.tripService.cancelTripFromApi(this.tripId)
+                })
+            ).subscribe(
+                (data: Trip) => {
+                    console.log(data)
+                    this.loadingService.isLoadingLine.next(false);
+                    this.isLoading = false
+
+                    this.router.navigate(['/trip-cancel'], {queryParams: {tripId: data.tripId}})
+                }, () => {
+                    this.loadingService.isLoadingLine.next(false);
+                    this.isLoading = false;
+                    this.isError = "An Error has occurred"
+                }, () => {
+                    this.loadingService.isLoadingLine.next(false);
+
+                    this.isLoading = false;
+                })
+
+
+
+    }
+
+    ngAfterViewInit() {
+        this.calculateTripCost()
+        console.log(this.trip)
+
+    }
+
+    intialiseSuccessStatus(): void {
+        this.route.queryParams.subscribe((queryParams) => {
+            this.isNewlyBooked = queryParams['success'];
+            if (this.isNewlyBooked) {
+                this.toastr.success('Trip Booked Successfully', '', {
+                    positionClass: 'toast-top-center'
+                });
+            }
+
+        }, error1 => {
+            console.log("error", error1)
+        })
+    }
+
+    initTripId() {
+        this.isLoading = true;
+        console.log("loading service placeholder")
+        this.loadingService.isLoading.next(true)
+        this.route.params.subscribe(params => {
+            this.tripId = params['id'];
+            console.log(this.tripId)
+            this.fetchTripDetails(this.tripId);
+        })
+    }
+
+    fetchTripDetails(id: number) {
+        this.isLoading = true;
+        console.log("loading service placeholder")
+        this.loadingService.isLoading.next(true)
+        console.log(id)
+        this.tripService.fetchTripDetailsFromApi(id).pipe(delay(9000)).subscribe((tripData: any) => {
+            this.tripEndFormattedDate = new Date(tripData.tripEnd);
+            this.trip = tripData;
+            this.loadingService.isLoading.next(false)
+            this.isLoading = false
+            setTimeout(()=>{
+
+                this.initMap()
+            },1000)
+        },
+            ()=>{
+                this.loadingService.isLoading.next(false)
+                this.isLoading = false
+
+            })
+
+    }
+
+    calculateTripCost() {
+        console.log(this.trip.days)
+        this.initialCost = this.trip.days * this.trip.eScooterOnTrip.cost;
+        this.vatCost = this.initialCost * 0.20;
+        this.totalCost = this.initialCost + 20 + this.vatCost;
+    }
+
+    initMap() {
+        var location = {lat: 53.410980, lng: -6.400090}
+        var options = {
+
+            center: location,
+            zoom: 15
+        }
+
+        const map = new google.maps.Map(document.getElementById("map"), options)
+
+        new google.maps.Marker({
+            position: location,
+            map,
+            title: "Hello World!",
+            icon: "assets/images/website/icons/markerChanged.png"
+        });
+    }
 
     onCompleteTrip() {
-      this.openCompleteDialog("complete-trip")
+        this.openCompleteDialog("complete-trip")
     }
 
-    handleConfirm(event:Event){
-    console.log(event)
+    handleConfirm(event: Event) {
+        console.log(event)
     }
 
-  openCompleteDialog(dialogType:String): void {
-    const dialogRef = this.dialog.open(AreYouSureDialogComponent,{
-      height: '200px',
-      width: '700px',
-      data: {
-        type: dialogType
-      }
-    });
+    openCompleteDialog(dialogType: String): void {
+        const dialogRef = this.dialog.open(AreYouSureDialogComponent, {
+            height: '200px',
+            width: '700px',
+            data: {
+                type: dialogType
+            }
+        });
 
-    console.log(dialogRef)
+        console.log(dialogRef)
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
 
-  openCancelDialog(dialogType:String): void {
-    const dialogRef = this.dialog.open(AreYouSureDialogCancelComponent,{
-      height: '200px',
-      width: '700px',
-      data: {
-        type: dialogType
-      }
-    });
+    openCancelDialog(dialogType: String): void {
+        const dialogRef = this.dialog.open(AreYouSureDialogCancelComponent, {
+            height: '200px',
+            width: '700px',
+            data: {
+                type: dialogType
+            }
+        });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-    });
-  }
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
 
 
+    onCancelTrip() {
+        this.openCancelDialog("complete-trip")
 
-  onCancelTrip() {
-    this.openCancelDialog("complete-trip")
-
-  }
+    }
 }
